16791c9c74f1dde6bd444baeccca45c98ed0b44c
amitsingh771087 <amitsingh77108792@gmail.com>
2025-08-26 13:13:34 +0530

    connceted DB with neon

---
+++ Backend/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { sql } from './config/db.js';
+
+const app = express();
+dotenv.config();
+const Port  = process.env.PORT || 9001;
+
+app.use(cors());
+app.use(express.json());
+
+async function initDB() {
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount decimal(10,2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`
+
+        console.log("DB initialized successfully")
+
+    } catch (error) {
+        console.log("error while initializing database ", error);
+        process.exit(1);
+        
+    }
+    
+}
+
+app.get('/' ,(req,res)=>{
+    res.send("Hello World")
+} )
+
+
+initDB().then(()=>{
+    app.listen(Port ,()=>{
+    console.log("server is running on port  : ", Port)
+})
+})
+
