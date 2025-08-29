8b3b3269d9f667e671fa853803f9fa439c9763c4
amitsingh771087 <amitsingh77108792@gmail.com>
2025-08-29 12:10:46 +0530

    deploy backend and added cron for schedule task for Backend cannot sleep

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,11 +5,18 @@ import initDB from './model/transaction.js';
 
 import transactionRoutes from './router/transactionRoutes.js';
 import rateLimiter from './middleware/rateLimiter.js';
+import job from './config/cron.js';
 
 
 const app = express();
 dotenv.config();
+
+if(process.env.NODE_ENV === "production") job.start()
+
+
 const Port  = process.env.PORT || 9001;
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -18,7 +25,9 @@ app.use(rateLimiter);
 app.use('/api', transactionRoutes)
 
 
-
+app.get('/api/health' , (req,res)=>{
+    res.status(200).json({status:"ok",message : "API is healthy"})
+})
 app.get('/' ,(req,res)=>{
     res.send("Hello World")
 } )
